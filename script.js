9617e4de438f816919aa3488d815e97e96ab65e8
CI5co22 <arnoldajcip225@gmail.com>
2025-09-18 11:14:02 -0600

    complicandome mucho...

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// pregunta 2 
+
+let a = 42;
+let b = "Hola mundo";
+let c = true;
+let d = null;
+let e = undefined;
+let f = [1, 2, 3];
+let g = {nombre: "Juan"};
+
+console.log("a:", typeof a); 
+console.log("b:", typeof b);
+console.log("c:", typeof c);
+console.log("d:", typeof d);
+console.log("e:", typeof e);
+console.log("f:", typeof f);
+console.log("g:", typeof g); 
